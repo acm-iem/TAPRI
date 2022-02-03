2c506afabdc212407e21643fcc6da10de8a57e08
naveen8801 <naveensharma10d@gmail.com>
2022-02-03 20:23:18 +0530

    404 Page improoved

--- frontend/src/Component/NotFound.jsx
+++ frontend/src/Component/NotFound.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React from 'react';
+import { makeStyles, Card, CardMedia } from '@material-ui/core';
+import img from './../assets/404img.png';
 
-const NotFound =(()=>{
-    return(
-        <div>
-            <h1>Not Found</h1>
-        </div>
-    )
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    height: '90vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    padding: '1rem',
+    width: '30%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: '16px',
+    textAlign: 'center',
+  },
 });
+
+const NotFound = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardMedia component="img" image={img} alt="Paella dish" />
+        <h5 className={classes.text}>
+          Sorry, the page you are looking for is now beyond our reach.
+        </h5>
+      </Card>
+    </div>
+  );
+};
 
 export default NotFound;
